0c9614e77076c54eee3a3f9cd0c596104ccba5c8
Chaz Corlew <chaz.corlew@gmail.com>
2022-08-29 16:27:42 -0700

    PUSH!

---
+++ p0/src/main/Driver.java
@@ -0,0 +1,14 @@
+
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		TheGame g = new TheGame();
+		g.gameMenu();
+		BaseballQuiz b = new BaseballQuiz();
+		b.start();
+
+	}
+
+}
